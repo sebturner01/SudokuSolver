763ef90147489724103f8e4146a94743462d4c30
sebturner01 <sebturner01@gmail.com>
2019-08-26 20:39:56 -0400

    first final version of sudoku board

--- sudokuBoard.c
+++ sudokuBoard.c
@@ -15,7 +15,7 @@
  * in README.md.
  * 
  * Access to this 2d array of cells is done in the way a matrix is 
- * traditionally examined mathamatically. That is all declarations are in
+ * traditionally examined in linear algebra. That is all declarations are in
  * [row][col] format.
  * 
  */
@@ -25,24 +25,29 @@
 #include <string.h>
 #include <ctype.h>
 
-#define BOARDSIZE 9 //sudoku boards are sqaure so only one value is needed 
+#define BOARDSIZE 9 //sudoku boards are square so only one value is needed 
 
 typedef struct cell{
     int value; //The current value in the cell
-    bool clue; //Whether or not the given value was an original one
+    bool clue; //Whether or not the given value was given as a clue
 }Cell;
 
-//function prototypes 
+/********* function prototypes *********/ 
+
+//Initialization, setters, and getters
 Cell **initBoard();    //initialzes an empty board
 Cell **initSetBoard(char *clues); //intializes a board from a given clue set
 Cell *getCell(int row, int col, Cell **board);
+void setCellVal(Cell **board, int row, int col, int val);
 
-bool checkBoard(Cell **board); //Checks a given board for legality
-bool isLegalCell(Cell *cell);
-bool checkRow(Cell **board, int row);
-bool checkCol(Cell **board, int col);
-bool checkSquare(Cell **board);
+//Legality functions
+bool checkBoard(Cell **board); 
+bool isLegalCell(Cell **board, int row, int col);
+bool isLegalRow(Cell **board, int row);
+bool isLegalCol(Cell **board, int col);
+bool isLegalSquare(Cell **board, int row, int col);
 
+//General functions
 void printBoard();
 void deleteBoard();
 
@@ -56,7 +61,7 @@ void deleteBoard();
  */ 
 Cell** initBoard()
 {
-    Cell** board;
+    Cell** board = calloc(BOARDSIZE, sizeof(Cell));
     for(int i = 0; i < BOARDSIZE; i++){
         board[i] = calloc(BOARDSIZE, sizeof(Cell));
         if(board[i] == NULL){
@@ -72,6 +77,7 @@ Cell** initBoard()
             board[i][j] = *cell;
         }
     }
+    return board;
 }
 
 /**
@@ -93,7 +99,7 @@ Cell** initSetBoard(char *clues)
     //checking that all given values are numerical
     for(int i = 0; i < len; i++){
         char c = clues[i];
-        if(!isdigit){
+        if(!isdigit(c)){
             fprintf(stderr, "The given clue sequence contains the non-numeric char %c", c);
             return NULL;
         }
@@ -106,7 +112,8 @@ Cell** initSetBoard(char *clues)
         for(int col = 0; col < BOARDSIZE; col++){
             Cell *curCell = getCell(row, col, board);
             curCell->clue = true;
-            int cellVal = atoi(clues[loc]);
+            const char charVal = clues[loc];
+            int cellVal = atoi(&charVal);
             curCell->value = cellVal;
             loc++;
         }
@@ -137,6 +144,53 @@ Cell *getCell(int row, int col, Cell** board)
 }
 
 /**
+ * Sets the cell at the given [row][col] to the given value. This function
+ * will do nothing if the given board is NULL, either the row or col argument
+ * is not within the board or if the given value is not within 1 - 9.
+ */ 
+void setCellVal(Cell **board, int row, int col, int val)
+{
+    if(board == NULL ||
+    row < 0 || row >= BOARDSIZE ||
+    col < 0 || col >= BOARDSIZE ||
+    val < 1 || val > 9){
+        return;
+    }
+
+    Cell *cell = getCell(row, col, board);
+    if(cell == NULL){
+        return;
+    }
+
+    cell->value = val;
+}
+
+/**
+ * Checks if a the cell at the given [row][col] position has a legal value given
+ * the relvant row, column, and square it is in. 
+ * 
+ * This function will return false if the isLegalRow, isLegalCol, or the
+ * isLegalSquare tests fail. In addition this fucntion will return false if 
+ * the given [row][col] position is not within the board or if the given board 
+ * is NULL.
+ */ 
+bool isLegalCell(Cell** board, int row, int col)
+{
+    if(board == NULL ||
+       row < 0 || row >= BOARDSIZE ||
+       col < 0 || col >= BOARDSIZE){
+        return false;
+    }
+
+    if(!(isLegalCol(board, col) || 
+         isLegalRow(board, row) ||
+         isLegalSquare(board, row, col))){
+        return false;
+    }
+    return true;
+}
+
+/**
  * Checks the given sudoku board for legality of position. If the position is 
  * not legal then this function will return false. This function will also 
  * return false in the case that the given board is NULL or if any of cells 
@@ -149,14 +203,36 @@ bool checkBoard(Cell **board) //TO-DO finish ALL legality functions
         return false;
     }
 
+    for(int index = 0; index < BOARDSIZE; index++){
+        if(!isLegalRow(board, index)){
+            return false;
+        }
+        if(!isLegalCol(board, index)){
+            return false;
+        }
+    }
+
+    //Only need to check a total of 9 squares which can be referenced by their
+    //upper left hand cells
+    for(int row = 0; row < BOARDSIZE; row += 3){
+        for(int col = 0; col < BOARDSIZE; col += 3){
+            if(!isLegalSquare(board, row, col)){
+                return false;
+            }
+        }
+    }
+    return true;
+
 } 
 
 /**
  * Checks if a given row is legal. That is that the row contains only unique 
- * numbers. This function will return false if the board is not initialized or 
+ * numbers. If only unique numbers are found then this fucntion returns true.
+ * 
+ * This function will return false if the board is not initialized or 
  * if row is less than 0 or greater or equal to BOARDSIZE.
  */ 
-bool checkRow(Cell **board, int row)
+bool isLegalRow(Cell **board, int row)
 {
     if(board == NULL || row < 0 || row >= BOARDSIZE){
         return false;
@@ -167,13 +243,12 @@ bool checkRow(Cell **board, int row)
         if(num == 0){  //Never need to check uninitialized values
             continue;
         }
-        int j = i + 1; //We want to check all other values in the row but never
-                       //the original value. In addition we never need to 
-                       //"recheck" a given value pair. 
 
-        
-        for(j; j < BOARDSIZE; j++){
-            int checkNum = getCell(row, j, board)->value;
+        //We want to check all other values in the row but never the original 
+        //value. In addition we never need to "recheck" a given value pair. 
+
+        for(int col = i + 1; col < BOARDSIZE; col++){
+            int checkNum = getCell(row, col, board)->value;
             if(checkNum == 0){ //Never need to check unitialized values.
                 continue;
             }
@@ -186,14 +261,15 @@ bool checkRow(Cell **board, int row)
 }
 
 /**
- * Checks if a given coluk=mn is legal. That is that each cell only contains 
- * unique values between 1 - 9.
+ * Checks if a given column is legal. That is that each cell only contains 
+ * unique values between 1 - 9. If that is the case then this funciton returns
+ * true.
  * 
  * This function will return false if:
  * a. The board is not initialzed
  * b. col is < 0 or greater than or equal to BOARDSIZE.
  */ 
-bool checkCol(Cell **board, int col)
+bool isLegalCol(Cell **board, int col)
 {
     if(board == NULL || col < 0 || col >= BOARDSIZE){
         return NULL;
@@ -204,13 +280,12 @@ bool checkCol(Cell **board, int col)
         if(num == 0){  //Never need to check uninitialized values
             continue;
         }
-        int j = i + 1; //We want to check all other values in the row but never
-                       //the original value. In addition we never need to 
-                       //"recheck" a given value pair. 
+         //We want to check all other values in the row but never the original 
+         //value. In addition we never need to "recheck" a given value pair. 
 
         
-        for(j; j < BOARDSIZE; j++){
-            int checkNum = getCell(j, col, board)->value;
+        for(int row = i + 1; row < BOARDSIZE; row++){
+            int checkNum = getCell(row, col, board)->value;
             if(checkNum == 0){ //Never need to check unitialized values.
                 continue;
             }
@@ -220,6 +295,93 @@ bool checkCol(Cell **board, int col)
         }
     }
     return true;
+}
+
+/**
+ * Checks if a given square is legal in the sudoku position. A square may be 
+ * referenced by any Cell in it's 3x3 area. 
+ * 
+ * This function returns false if any duplicate values are found within the 
+ * referenced square or if the given board is NULL, or the x,y position given 
+ * is not within the sudoku board. 
+ * 
+ * Otherwise this function will return true.
+ */ 
+bool isLegalSquare(Cell **board, int row, int col)
+{
+    if(board == NULL ||
+       row < 0 || row >= BOARDSIZE ||
+       col < 0 || col >= BOARDSIZE){
+           return false;
+    }
+    int refRow = row - (row % 3); //Gives a row and col value that are easy to 
+    int refCol = col - (col % 3); //work from. This references the top lefthand
+                                  //cell in a given square
+
+    int maxRow = refRow + 3;
+    int maxCol = refCol + 3;
+
+    for(int i = refRow; i < maxRow; i++){ 
+        for(int j = refCol; j < maxCol; j++){
+            int num = getCell(row, col, board)->value;
+            if(num == 0){ //No need to check unitialized values
+                continue;
+            }
+
+            for(int sRow = i; sRow < maxRow; sRow++){
+                for(int sCol = j; sCol < maxCol; sCol++){
+                    int checkNum = getCell(sRow, sCol, board)->value;
+                    if(checkNum == 0){ //No need to check unitialzed values
+                        continue;
+                    }
+                    if(num == checkNum){
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+
+    return true;
+}
+
+
+/**
+ * Prints a version of the sudoku board to the console. This function will not
+ * change anything about the given board. If the given board is NULL this 
+ * function will not print anything
+ */ 
+void printBoard(Cell **board)
+{
+    if(board == NULL){
+        return;
+    }
+
+    //Printing some reference coordinates for columns
+    for(int i = 0; i < BOARDSIZE; i++){
+        printf(" %d ", i);
+    }
+
+    printf("\n");
+
+    //First seperator line
+    for(int i = 0; i < BOARDSIZE; i++){
+        printf("___");
+    }
+
+    printf("\n");
+
+    for(int row = 0; row < BOARDSIZE; row++){
+        printf("%d", row);
+        for(int col = 0; col < BOARDSIZE; col++){
+            int val = getCell(row, col, board)->value;
+            printf("|%d|", val);
+        }
+        printf("\n");
+        for(int i = 0; i < BOARDSIZE; i++){
+            printf("___");
+        }
+    }
 }
 
 
@@ -242,4 +404,5 @@ void deleteBoard(Cell **board)
             }
         }
     }
+    free(board);
 }
